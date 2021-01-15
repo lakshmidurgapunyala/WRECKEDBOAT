afbd6e111ef06978faede11dbf2ec9f16f6de65c
root <lakshmidurgapunyala@gmail.com>
2021-01-16 01:37:51 +0530

    first commit

---
+++ src/main/java/com/example/DevOpsArtifact/DevOpsMvnProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.DevOpsArtifact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevOpsMvnProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevOpsMvnProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DevOpsArtifact/DevOpsMvnProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DevOpsArtifact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevOpsMvnProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
